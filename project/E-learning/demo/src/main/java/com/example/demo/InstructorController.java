9f2fb71fe8b887e692243b60bdd4e6e45578ddd3
Ananya <79403627+ananyaprakash2002@users.noreply.github.com>
2023-04-17 12:11:18 +0530

    Add files via upload
    Instructor,Learner,Login pages

---
+++ project/E-learning/demo/src/main/java/com/example/demo/InstructorController.java
@@ -0,0 +1,87 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class InstructorController
+{
+  @Autowired // MainController class can use the methods provided by the 
+  //InstructorService class without having to explicitly create an instance of it.
+  private InstructorService instructorService;
+  //display all the instructors
+  @GetMapping("/")
+  public String viewHomePage(Model model)
+  {
+    return "index";
+  }
+
+  @GetMapping("/LoginInstructorForm")
+  public String showNewInstructorForm(Model model)
+  {
+    //create model attribute to bind the form data
+    Instructor instructor = new Instructor();
+    //thymleaf template will access this empty instructor object for binding form data{"instructor"}
+    model.addAttribute("instructor",instructor);
+    return "instructor_login";
+  }
+
+   
+  @GetMapping("/instructor_register")
+    public String InstructorRegister(Model model) {
+        Instructor instructor = new Instructor();
+        model.addAttribute("instructor", instructor);
+        return "new_instructor";
+  }
+
+  @PostMapping("/saveInstructor")
+  public String saveInstructor(@ModelAttribute("instructor") Instructor instructor)
+  {
+    //save instructor to database
+    instructorService.saveInstructor(instructor);
+    return "redirect:/";
+  }
+
+@GetMapping("/instructor_login")
+    public String InstructorLogin(Model model) 
+    {
+        Instructor instructor = new Instructor();
+        model.addAttribute("instructor", instructor);
+        return "instructor_login";
+    }
+@PostMapping("/instructor_login")
+public String handleInstructorLogin(@ModelAttribute("instructor") Instructor instructor) 
+{
+      
+        if (instructorService.isValidInstructor(instructor.getUsername(),instructor.getPassword())) {
+            return "instructor_functions";
+        } else {
+            return "instructor_login";
+        }
+    } 
+
+@GetMapping("/instructor_functions")
+public String InstructorFunctions(Model model, @RequestParam("username") String username) 
+    {
+        // get instructor from the service based on the username
+        Instructor instructor = instructorService.getInstructorByUsername(username);
+        // create model attribute to bind the form data
+        model.addAttribute("instructor", instructor);
+        return "instructor_functions";
+    }
+    @GetMapping("/updateinstructor")
+    public String showFormForUpdate(Model model, @RequestParam("username") String username) {
+        // get instructor from the service based on the username
+        Instructor instructor = instructorService.getInstructorByUsername(username);
+        // create model attribute to bind the form data
+        model.addAttribute("instructor", instructor);
+        return "update_instructor";
+    }
+}
+
---
+++ project/E-learning/demo/src/main/java/com/example/demo/InstructorServiceImp.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+//A service component is a class that 
+//contains business logic and performs some specific operations in an application. 
+//@Service annotation,we are instructing the Spring Framework to automatically create 
+//an instance of the class at runtime and manage its lifecycle.
+public class InstructorServiceImp implements InstructorService 
+{
+    @Autowired
+    private InstructorRepository instructorRepository;
+
+    @Override
+    public List<Instructor> getAllInstructors()
+    {
+        return instructorRepository.findAll();
+    }
+
+    @Override
+    public void saveInstructor(Instructor instructor)
+    {
+        this.instructorRepository.save(instructor); //save instructor to the database
+    }
+
+    @Override
+    public Instructor getInstructorById(Integer id)
+    {
+        Optional<Instructor> optional = instructorRepository.findById(id);
+        Instructor instructor = null;
+        if(optional.isPresent())
+        {
+            instructor = optional.get();
+        }
+        else
+        {
+            throw new RuntimeException("Instructor not found for id::" + id);
+        }
+        return instructor;
+    }
+
+    @Override
+    public void deleteInstructorById(Integer id)
+    {
+        this.instructorRepository.deleteById(id);
+    }
+
+    @Override
+    public Instructor getInstructorByUsername(String username) {
+        return instructorRepository.findByUsername(username);
+     }
+    
+    @Override
+    public boolean isValidInstructor(String username, String password) {
+        // retrieve the instructor object by username
+        Instructor instructor = getInstructorByUsername(username);
+    
+        // check if the instructor object is not null and password matches
+        if (instructor != null && instructor.getPassword().equals(password)) {
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean isValidPassword(String password) {
+        // password should have at least 8 characters and contain at least one digit, lowercase letter, and uppercase letter
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+        return password.matches(passwordRegex);
+    }
+}
