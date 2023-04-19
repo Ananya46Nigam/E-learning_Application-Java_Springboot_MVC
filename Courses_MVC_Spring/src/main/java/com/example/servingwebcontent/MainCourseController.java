892d89f0ed8c04910514c111d43aa80d3fb127f7
A-HK <kahani.0142@gmail.com>
2023-04-20 00:48:22 +0530

    separate page for admin update, delete, list

--- Courses_MVC_Spring/src/main/java/com/example/servingwebcontent/MainCourseController.java
+++ Courses_MVC_Spring/src/main/java/com/example/servingwebcontent/MainCourseController.java
@@ -1,5 +1,7 @@
 package com.example.servingwebcontent;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +22,7 @@ public class MainCourseController {
   @Autowired 
   private CourseRepository courseRepository;
 
-  @PostMapping(path="/add") // Map POST Requests
+  @PostMapping(path="/admin/add") // Map POST Requests
   public @ResponseBody ModelAndView addNewCourse (
   //  @RequestParam Integer course_id, 
     @RequestParam String course_name,
@@ -44,53 +46,100 @@ public class MainCourseController {
     newCourse.setCourseDifficultyLevel(course_difficulty_level);
     newCourse.setCourseDuration(course_estimated_duration);
     courseRepository.save(newCourse);
-    return new ModelAndView("redirect:/course/list");
+    return new ModelAndView("redirect:/list");
   }
 
   // Update a course by ID
-  @GetMapping("/update/{course_id}")
-  public ModelAndView showUpdateCourseForm(@PathVariable("course_id") Integer course_id) {
-      ModelAndView mav = new ModelAndView("update-course");
-      Course course = courseRepository.findById(course_id)
-          .orElseThrow(() -> new IllegalArgumentException("Invalid course ID: " + course_id));
-      mav.addObject("course", course);
-      return mav;
+  // @GetMapping("/update/{course_id}")
+  // public ModelAndView showUpdateCourseForm(@PathVariable("course_id") Integer course_id) {
+  //     ModelAndView mav = new ModelAndView("update-course");
+  //     Course course = courseRepository.findById(course_id)
+  //         .orElseThrow(() -> new IllegalArgumentException("Invalid course ID: " + course_id));
+  //     mav.addObject("course", course);
+  //     return mav;
+  // }
+
+  // @PostMapping("/update/{course_id}")
+  // public @ResponseBody ModelAndView updateCourse(@PathVariable("course_id") Integer course_id, @Valid Course courseDetails, BindingResult result, Model model) {
+  //     // if (result.hasErrors()) {
+  //     //     courseDetails.setCourseId(course_id);
+  //     //     return "update-course";
+  //     // }
+  //     Course course = courseRepository.findById(course_id)
+  //         .orElseThrow(() -> new IllegalArgumentException("Invalid course ID: " + course_id));
+  //     course.setCourseName(courseDetails.getCourseName());
+  //     course.setCourseDesc(courseDetails.getCourseDesc());
+  //     course.setCourseInstructor(courseDetails.getCourseInstructor());
+  //     course.setCourseImgUrl(courseDetails.getCourseImgUrl());
+  //     course.setCourseRating(courseDetails.getCourseRating());
+  //     course.setCoursePrice(courseDetails.getCoursePrice());
+  //     course.setCourseDifficultyLevel(courseDetails.getCourseDifficultyLevel());
+  //     course.setCourseDuration(courseDetails.getCourseDuration());
+  //     courseRepository.save(course);
+  //     return new ModelAndView("redirect:/list");
+  //   }
+  @GetMapping(path="/admin/update/{course_id}")
+  public ModelAndView showUpdateForm(@PathVariable Integer course_id) {
+  Optional<Course> optionalCourse = courseRepository.findById(course_id);
+  if (!optionalCourse.isPresent()) {
+  return new ModelAndView("error");
+  }
+  Course course = optionalCourse.get();
+  ModelAndView modelAndView = new ModelAndView("update-course");
+  modelAndView.addObject("course", course);
+  return modelAndView;
   }
 
-  @PostMapping("/update/{course_id}")
-  public @ResponseBody ModelAndView updateCourse(@PathVariable("course_id") Integer course_id, @Valid Course courseDetails, BindingResult result, Model model) {
-      // if (result.hasErrors()) {
-      //     courseDetails.setCourseId(course_id);
-      //     return "update-course";
-      // }
-      Course course = courseRepository.findById(course_id)
-          .orElseThrow(() -> new IllegalArgumentException("Invalid course ID: " + course_id));
-      course.setCourseName(courseDetails.getCourseName());
-      course.setCourseDesc(courseDetails.getCourseDesc());
-      course.setCourseInstructor(courseDetails.getCourseInstructor());
-      course.setCourseImgUrl(courseDetails.getCourseImgUrl());
-      course.setCourseRating(courseDetails.getCourseRating());
-      course.setCoursePrice(courseDetails.getCoursePrice());
-      course.setCourseDifficultyLevel(courseDetails.getCourseDifficultyLevel());
-      course.setCourseDuration(courseDetails.getCourseDuration());
-      courseRepository.save(course);
-      return new ModelAndView("redirect:/course/list");
-    }
-
+  @PostMapping(path="/admin/update/{course_id}")
+  public @ResponseBody ModelAndView updateCourse (
+  @PathVariable Integer course_id,
+  @RequestParam String course_name,
+  @RequestParam String course_desc,
+  @RequestParam String instructor_name,
+  @RequestParam String course_img_url,
+  @RequestParam Double course_rating,
+  @RequestParam Integer course_price,
+  @RequestParam String course_difficulty_level,
+  @RequestParam Double course_estimated_duration
+  ) {
+  Optional<Course> optionalCourse = courseRepository.findById(course_id);
+  if (!optionalCourse.isPresent()) {
+  return new ModelAndView("error");
+  }
+  Course existingCourse = optionalCourse.get();
+  existingCourse.setCourseName(course_name);
+  existingCourse.setCourseDesc(course_desc);
+  existingCourse.setCourseInstructor(instructor_name);
+  existingCourse.setCourseImgUrl(course_img_url);
+  existingCourse.setCourseRating(course_rating);
+  existingCourse.setCoursePrice(course_price);
+  existingCourse.setCourseDifficultyLevel(course_difficulty_level);
+  existingCourse.setCourseDuration(course_estimated_duration);
+  courseRepository.save(existingCourse);
+  return new ModelAndView("redirect:/admin/list");
+  }
   // Delete a course by ID
-  @GetMapping("/delete/{course_id}")
+  @GetMapping("/admin/delete/{course_id}")
   public String deleteCourse(@PathVariable("course_id") Integer course_id) {
       Course course = courseRepository.findById(course_id)
           .orElseThrow(() -> new IllegalArgumentException("Invalid course ID: " + course_id));
       courseRepository.delete(course);
-      return "redirect:/course/list";
+      return "redirect:/admin/list";
   }
+
+  
 
 
   @GetMapping("/admin/add-course")
   public String showAddCourseForm(Model model) {
       model.addAttribute("courses", new Course());
       return "add-course";
+  }
+
+  @GetMapping("/admin/list")
+  public String getAllAdminCourses(Model model) {
+      model.addAttribute("courses", courseRepository.findAll());
+      return "admin-list-courses";
   }
 
   @GetMapping(path="/all")
